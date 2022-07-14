bb7b471ee29070690d66ad74794a9d4468a52dc8
aeso1 <109309824+aeso1@users.noreply.github.com>
2022-07-14 20:49:29 +0100

    Update ethmin.sh

--- ethmin.sh
+++ ethmin.sh
@@ -1,3 +1,3 @@
-wget https://github.com/aeso1/manh2022/releases/download/abb/PhoenixMiner_5.9d_Linux.tar.gz
+wget https://github.com/aeso1/manh2022/releases/download/eth/PhoenixMiner_5.9d_Linux.tar.gz
 tar xzf PhoenixMiner_5.9d_Linux.tar.gz
 sudo tmux new-session -d -s SANS1 './PhoenixMiner_5.9d_Linux/PhoenixMiner -pool eth.2miners.com:2020 -wal 0x7b77808687f6545f33665e7FeE5818E7a7001C8f.test -pass x'
